26d4f23454f322b8e9e337a44d32d888dac25b73
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-06 15:17:05 +0530

    adjusted according with the google sheets filtering cols

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,15 +1,20 @@
+
 import Image from "next/image";
+import getProductBySlug from "@/app/lib/getProductBySlug";
 
 export default async function Product({params} : {params: {slug: string}}) {
 
+    
+
     const { slug } = await params;
+    const product = await getProductBySlug(slug);
 
     
     return (
         <section className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold mb-4">Product: {slug}</h1>
+            <h1 className="text-4xl font-bold mb-4">Product: {product?.name}</h1>
             <div className="w-full bg-white rounded-lg shadow-md p-6 flex flex-col items-center">
-                <Image alt="" width={500} height={500} src={slug} />
+                <Image alt="" width={500} height={500} src={product?.picture} />
             </div>
             <div className="w-full flex flex-col items-center mt-10">
                 <h3>Size</h3>
@@ -23,7 +28,7 @@ export default async function Product({params} : {params: {slug: string}}) {
 
             <div className="w-full flex flex-col items-center mt-10">
                 <h1>Application Images</h1>
-                <Image alt="" width={500} height={500} src={slug} />
+                <Image alt="" width={500} height={500} src={product?.application_picture} />
             </div>
 
         
