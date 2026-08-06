7511cb2172cf92f22b139ec91330b85f07ae5b13
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-06 15:16:33 +0530

    added getProductsBy Slug fucntion

---
+++ app/lib/getProductBySlug.ts
@@ -0,0 +1,11 @@
+import getAllProducts from "./getProducts";
+
+export default async function getProductBySlug(slug: string) {
+    try {
+        const products = await getAllProducts();
+        return products.find((product) => product.slug === slug);
+        
+    } catch (error) {
+        console.error("Failed Fetching The Speicific Product", error)
+    }
+}
