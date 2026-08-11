dca105097c0fc94ed68afd18ff8eb7bd25f088bd
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-11 16:35:18 +0530

    added props for the searchBar

--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -1,16 +1,20 @@
 'use client';
 import { useState } from 'react';
-export default function SearchBar() {
-    const [inputValue, setInputValue] = useState('');
+
+type SearchBarProps = {
+    searchQ: string
+    setSearchQ: (value: string) => void;
+}
+export default function SearchBar({searchQ, setSearchQ} : SearchBarProps) {
     return (
         <section>
             <div className="flex justify-center">
                 <input
                     type="text"
                     placeholder="Enter products, brands, or categories..."
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                    className="border border-gray-300 rounded-md px-4 py-2 w-full max-w-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    value={searchQ}
+                    onChange={(e) => setSearchQ(e.target.value)}
+                    className="bg-white border border-gray-300 rounded-md px-4 py-2 w-full max-w-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
                 />
             </div>
         </section>
