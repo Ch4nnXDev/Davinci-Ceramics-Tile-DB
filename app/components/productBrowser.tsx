6584b515b2e37f58090e8c3f71015144ea7a2deb
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-11 16:33:56 +0530

    created a combined component of the seaechbar and the product grid as a cleint side component

---
+++ app/components/productBrowser.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { Product } from "./../types/product";
+import ProductGrid from "./productGrid";
+import SearchBar from "./searchBar";
+import {useState} from 'react';
+import filterProducts from "../lib/filterProducts";
+
+export default function ProductBrowser({products} : {products: Product[]}) {
+
+    const [searchQ, setSearchQ] = useState("");
+
+    const filteredProducts  = filterProducts(products, searchQ);
+
+    return (
+        <section>
+
+            <SearchBar searchQ={searchQ} setSearchQ={setSearchQ} />
+
+            <ProductGrid products={filteredProducts}/>
+        </section>
+    )
+
+
+}
