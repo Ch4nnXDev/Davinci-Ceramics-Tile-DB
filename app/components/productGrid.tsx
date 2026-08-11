a150e8904296de1cde267d2ee393078f0becb320
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-11 15:21:31 +0530

    the product grid turned to a client component

---
+++ app/components/productGrid.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Product } from "./../types/product";
+import ProductCard from "./product";
+
+export default function ProductGrid({products}: {products: Product[]}) {
+    return (
+        <section className="p-10 w-full">
+            
+
+                <div className="
+                    max-w-7xl 
+                    mx-auto
+                    grid
+                    grid-cols-1
+                    sm:grid-cols-2
+                    md:grid-cols-3
+                    lg:grid-cols-4
+                    gap-8"
+                >
+                {products.map((product: Product) => (
+                    <ProductCard product={product}/>
+                
+
+            ))}
+
+            </div>
+        </section>
+    )
+}
