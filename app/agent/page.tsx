f06ad2e49a815aba772493b70c1cae72ff16c556
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-12 11:26:54 +0530

    agen page skeleton done

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -1,3 +1,14 @@
-export default function StudyPage() {
+
+export default function AgentPage() {
+    const [prompt, setPrompt] = 
+    return (
+        <section className="flex flex-col h-screen bg-white">
+
+            <form>
+
+            </form>
+
+        </section>
+    )
     
 }
