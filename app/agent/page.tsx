65aba52534ac4c7d2e395133a9c5d9b7a11ffac1
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-12 15:20:23 +0530

    code cleanup

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -1,10 +1,14 @@
-
+'use client'
+import {useState} from 'react';
 export default function AgentPage() {
-    const [prompt, setPrompt] = 
+    const [prompt, setPrompt] = useState("");
     return (
         <section className="flex flex-col h-screen bg-white">
 
             <form>
+                <input 
+                value="text"
+                />
 
             </form>
 
