f83d9c9936138a49361d4ad62553888ba036df46
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-12 11:26:07 +0530

    seperated tiel calculator hook

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,82 +1,10 @@
 "use client";
-
-import { useState } from "react";
+import useTileCalculator from "../lib/tileCalculator";
 import {
   Calculator as CalculatorIcon,
   Ruler,
 } from "lucide-react";
 
-const MM_PER_FOOT = 304.8;
-
-
-
-function useTileCalculator() {
-  const [tileLength, setTileLength] = useState("");
-  const [tileWidth, setTileWidth] = useState("");
-
-  const [roomLength, setRoomLength] = useState("");
-  const [roomWidth, setRoomWidth] = useState("");
-
-  const [wastage, setWastage] = useState(10);
-
-  /*
-    Convert tile dimensions
-    from millimeters → feet
-  */
-
-  const tileLengthFt =
-    Number(tileLength) > 0
-      ? Number(tileLength) / MM_PER_FOOT
-      : 0;
-
-  const tileWidthFt =
-    Number(tileWidth) > 0
-      ? Number(tileWidth) / MM_PER_FOOT
-      : 0;
-
-
-
-  const tileArea = tileLengthFt * tileWidthFt;
-
-  
-
-  const roomArea =
-    Number(roomLength) > 0 && Number(roomWidth) > 0
-      ? Number(roomLength) * Number(roomWidth)
-      : 0;
-
-
-  const totalArea =
-    roomArea * (1 + wastage / 100);
-
-
-  const requiredTiles =
-    tileArea > 0
-      ? Math.ceil(totalArea / tileArea)
-      : 0;
-
-  return {
-    tileLength,
-    setTileLength,
-
-    tileWidth,
-    setTileWidth,
-
-    roomLength,
-    setRoomLength,
-
-    roomWidth,
-    setRoomWidth,
-
-    wastage,
-    setWastage,
-
-    tileArea,
-    roomArea,
-    totalArea,
-    requiredTiles,
-  };
-}
 
 
 
@@ -327,9 +255,7 @@ export default function Calculator() {
   );
 }
 
-/* --------------------------------
-   Input Component
---------------------------------- */
+
 
 function Input({
   label,
