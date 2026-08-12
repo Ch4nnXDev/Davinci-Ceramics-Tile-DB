f113bb8667a45cede54c126070b1158c104c40b5
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-12 15:20:49 +0530

    chatBar

---
+++ app/components/chatBar.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { ArrowUp } from 'lucide-react';
+import {useState} from "react";
+
+export default function chatBar() {
+    const [prompt, setPrompt] = useState("");
+    
+
+    <form>
+        <input
+            type="text"
+            placeholder="Enter a task or a query"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            className="bg-white border border-gray-300 rounded-md px-4 py-4 w-full max-w-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+
+        />
+        <button className="p-10 rounded-lg bg-gray-400" >
+            <ArrowUp 
+            size={23}
+            />
+
+        </button>
+    </form>
+    
+}
