041ecf35e4e740cab07cc9460ec5a6bf039f5c99
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-12 16:24:28 +0530

    next config changed to include nextjs Cache Components

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -1,8 +1,10 @@
 'use client'
-import {useState} from 'react';
+
 import ChatBar from '../components/chatBar';
+
+
 export default function AgentPage() {
-    const [prompt, setPrompt] = useState("");
+
     return (
         <section className="flex flex-col h-screen bg-white">
 
