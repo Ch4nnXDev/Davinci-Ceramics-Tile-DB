b617a68ff4229994e16cc091d67e6406d6ffbb21
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-12 16:57:17 +0530

    fix

--- app/lib/getProductBySlug.ts
+++ app/lib/getProductBySlug.ts
@@ -1,7 +1,7 @@
 import getAllProducts from "./getProducts";
 
 export default async function getProductBySlug(slug: string) {
-    "use cache"
+    "use cache";
     try {
         const products = await getAllProducts();
         return products.find((product) => product.slug === slug);
