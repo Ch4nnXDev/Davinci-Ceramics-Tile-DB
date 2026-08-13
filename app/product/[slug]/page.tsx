23e527e6d6fc5fa599d2a3cadec732f86b6a43e4
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-13 16:01:24 +0530

    made the dynmaic prpduct page dynamic such that it isnt pre-rendered and it fixes the need for suspense

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import getProductBySlug from "@/app/lib/getProductBySlug";
 
+export const dynamic = "force-dynamic";
+
 export default async function Product({params} : {params: {slug: string}}) {
 
     
