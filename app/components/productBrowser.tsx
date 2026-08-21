785c2d55fa35ba6b550e420dc7e2c452fc21be1c
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-21 15:29:34 +0530

    button fix

--- app/components/productBrowser.tsx
+++ app/components/productBrowser.tsx
@@ -26,7 +26,7 @@ export default function ProductBrowser({products} : {products: Product[]}) {
 
             <div className="ml-12">
                 <button
-                    className="mt-5 px-6 py-3 text-gray-700 border border-gray-500 rounded-lg hover:bg-gray-100"
+                    className="mt-2 px-3 py-3 text-gray-700 border border-gray-500 rounded-lg hover:bg-gray-100"
                     onClick={() => setVisibleCount((prev) => prev + 20)}
                 >
                     Load More
