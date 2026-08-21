7df38a7d977e51f8d6f43711717c94f2dc2ee848
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-21 15:32:36 +0530

    ux fix

--- app/components/productGrid.tsx
+++ app/components/productGrid.tsx
@@ -17,7 +17,7 @@ export default function ProductGrid({products}: {products: Product[]}) {
                     sm:grid-cols-2
                     md:grid-cols-3
                     lg:grid-cols-4
-                    gap-8"
+                    gap-2"
                 >
                 {products.map((product: Product, index) => (
                     
