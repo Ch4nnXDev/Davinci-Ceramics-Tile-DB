00d07f3d40806426412db0768649d77758ff06bf
Channa2146@gmail.com <Channa2146@gmail.com>
2026-08-25 11:10:39 +0530

    env fix

--- app/lib/Supabase/client.ts
+++ app/lib/Supabase/client.ts
@@ -5,8 +5,8 @@ import { createBrowserClient } from "@supabase/ssr";
 export default function createClient() {
 
     return createBrowserClient(
-        process.env.SUPABASE_URL!,
-        process.env.SUPABASE_PUBLISHABLE_KEY!
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
 
     );
 
